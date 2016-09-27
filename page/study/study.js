358a1d525b4571668efe2a58a75223ed02ddfd41
521zzp <1409905100@qq.com>
2016-09-27 18:38:27 +0800

    2016-09-27

--- page/study/study.js
+++ page/study/study.js
@@ -1,3 +1,9 @@
 Page({
-  
+   data: {
+    shopItems:[{"title":"清香芝麻香蕉","info":"有益健康有益健康","url":"../imgs/banana.png","num":10,},
+               {"title":"清香芝麻香蕉","info":"有益健康有益健康","url":"../imgs/banana.png","num":10,},
+               {"title":"清爽冰甜哈密瓜","info":"超级棒的美味","url":"../imgs/hami.png","num":50,},
+               {"title":"清爽冰甜哈密瓜","info":"超级棒的美味","url":"../imgs/hami.png","num":50,}],
+    change:"true"
+  }
 })
