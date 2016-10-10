1a2c74e7c55927ccbe751225d21088c0575d1346
521zzp <1409905100@qq.com>
2016-10-10 20:49:34 +0800

    直播页面模块化完成大部分，现在写title数据

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ App({
   globalData: {
     hasLogin: false
   },
-  user_face:"/images/noface.gif",
+  user_face:"/images/drawable-xxhdpi-v4/bili_default_avatar.png",
   user_name:"未登录",
 
 })
--- page/bilibili/recommend/recommend.js
+++ page/bilibili/recommend/recommend.js
@@ -4,7 +4,19 @@ Page({
   data:{
     // text:"这是一个页面"
     "user_face":app.user_face,
-    "user_name":app.user_name
+    "user_name":app.user_name,
+    "is_scroll-y":true,
+    live_siwpers:[{"imgpath":"/static/image/recommend_swiper/c89d65cb9c56d819c688464e712a38bb6e3dc3ac.jpg","url":"www.bilibili.com"},
+                    {"imgpath":"/static/image/recommend_swiper/785a892778b941f276a2208b68541be852445382.jpg","url":"www.bilibili.com"},
+                    {"imgpath":"/static/image/recommend_swiper/889c27bab8f2e2f7d412de9c8665fd9e48512f2f.jpg","url":"www.bilibili.com"},
+                    {"imgpath":"/static/image/recommend_swiper/89034a38a9415654004ecf66d6455cca8513830d.jpg","url":"www.bilibili.com"},
+                    {"imgpath":"/static/image/recommend_swiper/09c7f6f70de45e477ecdfc08b40cda77536ef996.jpg","url":"www.bilibili.com"}],
+    indicatorDots: true,  
+    vertical:false,
+    autoplay:true, 
+    siwper_interval:5000,
+    siwper_duration:1000,
+              
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
